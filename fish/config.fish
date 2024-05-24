6db2199f7dd8714fa50d4f2a2f46feb848690d2c
himanshuwiz <himdhi2003@gmail.com>
2024-05-25 00:41:23 +0530

    contains config file and all necessary files

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+     eval (starship init fish)    
+    # Commands to run in interactive sessions can go here
+end
+set -U fish_color_autosuggestion '555'
+ 
+
+#Aliases
+
+
+alias ll='ls -l'
+alias sd='poweroff'
+alias rs='restart'
+alias e='exit'
